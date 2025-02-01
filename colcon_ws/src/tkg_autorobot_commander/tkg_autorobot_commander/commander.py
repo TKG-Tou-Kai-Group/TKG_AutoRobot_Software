9b29eec35ae63ec3f830424a7973821d1b8b5be1
Masaaki Hijikata <hijimasa@gmail.com>
2025-02-02 03:12:23 +0900

    [add] add extract_object_region.py

--- colcon_ws/src/tkg_autorobot_commander/tkg_autorobot_commander/commander.py
+++ colcon_ws/src/tkg_autorobot_commander/tkg_autorobot_commander/commander.py
@@ -4,8 +4,13 @@ import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Vector3, PoseArray, Pose
 from std_msgs.msg import Float64, Int64, Empty, Bool
+from sensor_msgs.msg import Image, CameraInfo
+import numpy as np
+import cv2
+from cv_bridge import CvBridge
 
 from tkg_autorobot_commander.target_class import Target
+from tkg_autorobot_commander.extract_object_region import ExtractObject
 
 class Auto(Node):
 
@@ -15,9 +20,10 @@ class Auto(Node):
         self.POSE_OFFSET_X = 0.3
         self.POSE_OFFSET_Y = 0.0
 
-        self.AUTO_MODE_TRACKING = 0
-        self.AUTO_MODE_ATTACK = 1
-        self.auto_mode = self.AUTO_MODE_TRACKING
+        self.AUTO_MODE_INIT = 0
+        self.AUTO_MODE_TRACKING = 1
+        self.AUTO_MODE_ATTACK = 2
+        self.auto_mode = self.AUTO_MODE_INIT
 
         self.attack_counter = 0
 
@@ -29,6 +35,16 @@ class Auto(Node):
         self.control_pub = self.create_publisher(Bool, "/control", 10)
         self.yaw_pub = self.create_publisher(Float64, "/yaw", 10)
         self.pitch_pub = self.create_publisher(Float64, "/pitch", 10)
+
+        self.object_image_pub = self.create_publisher(Image, "/object_image", 10)
+
+        self.camera_mat = None
+        self.dist_coeffs = None
+        self.extractor = None
+        self.image = None
+        self.bridge = CvBridge()
+        self.camera_image_sub = self.create_subscription(Image,'/camera/camera/color/image_raw',self.image_callback,10)
+        self.camera_info_sub = self.create_subscription(CameraInfo,'/camera/camera/color/camera_info',self.camera_info_callback,10)
 
         self.poses = []
         self.target_list = []
@@ -45,6 +61,17 @@ class Auto(Node):
 
     def turret_callback(self, msg):
         self.turret_data = msg
+
+    def image_callback(self, msg):
+        try:
+            # ROS2のImageメッセージをOpenCVの画像に変換
+            self.image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+        except Exception as e:
+            self.get_logger().error(f"Failed to convert image: {e}")
+
+    def camera_info_callback(self, msg):
+        self.camera_mat = np.array(msg.k).reshape(3, 3)
+        self.dist_coeffs = np.array(msg.d)
 
     def clusters_callback(self, msg):
         if self.timer_processing == True:
@@ -106,7 +133,12 @@ class Auto(Node):
             if is_find == False:
                 self.selected_target_id = -1
 
+        if self.auto_mode == self.AUTO_MODE_INIT:
+            if self.camera_mat is not None and self.dist_coeffs is not None:
+                self.extractor = ExtractObject(self.camera_mat, self.dist_coeffs)
 
+                self.auto_mode = self.AUTO_MODE_TRACKING
+    
         if self.auto_mode == self.AUTO_MODE_TRACKING:
             self.get_logger().info(f"TRACKING_MODE:")
             if not self.selected_target_id == -1:
@@ -115,18 +147,25 @@ class Auto(Node):
                 self.yaw_pub.publish(Float64(data=float(target_yaw)))
                 self.pitch_pub.publish(Float64(data=float(target_pitch)))
                 if abs(target_yaw - current_yaw) <= math.radians(5.0) and abs(target_pitch - current_pitch) <= math.radians(5.0):
-                    # TODO : image processing
-                    (target_pitch, target_yaw, self.target_list[self.selected_target_index].state) = (target_pitch, target_yaw, Target.UNKNOWN)
-                    self.target_list[self.selected_target_index].state_checked_time = current_time
-                    if self.target_list[self.selected_target_index].state == Target.ENEMY:
-                        self.auto_mode = self.AUTO_MODE_ATTACK
-                    if self.target_list[self.selected_target_index].state == Target.OTHER:
-                        self.selected_target_id = -1
+                    if self.image is not None:
+                        extract_image, result_image = self.extractor.extract(self.image, self.target_list[self.selected_target_index].x, self.target_list[self.selected_target_index].y, current_pitch, current_yaw, (256, 128))
+                        ros_image = self.bridge.cv2_to_imgmsg(result_image, encoding='bgr8')
+                        self.object_image_pub.publish(ros_image)
+                        # TODO : image processing
+                        (target_pitch, target_yaw, self.target_list[self.selected_target_index].state) = (target_pitch, target_yaw, Target.UNKNOWN)
+                        self.target_list[self.selected_target_index].state_checked_time = current_time
+                        if self.target_list[self.selected_target_index].state == Target.ENEMY:
+                            self.auto_mode = self.AUTO_MODE_ATTACK
+                        if self.target_list[self.selected_target_index].state == Target.OTHER:
+                            self.selected_target_id = -1
                 self.get_logger().info(f"{self.target_list[self.selected_target_index].x},{self.target_list[self.selected_target_index].y},{target_yaw}, {target_pitch}")
 
         if self.auto_mode == self.AUTO_MODE_ATTACK:
             self.get_logger().info(f"ATTACK_MODE:")
             if not self.selected_target_id == -1:
+                extract_image, result_image = self.extractor.extract(self.image, self.target_list[self.selected_target_index].x, self.target_list[self.selected_target_index].y, current_pitch, current_yaw, (256, 128))
+                ros_image = self.bridge.cv2_to_imgmsg(result_image, encoding='bgr8')
+                self.object_image_pub.publish(ros_image)
                 # TODO: image processing and calclate target_pitch
                 (target_pitch, target_yaw, self.target_list[self.selected_target_index].state) = (target_pitch, target_yaw, Target.ENEMY)
                 self.target_list[self.selected_target_index].state_checked_time = current_time
