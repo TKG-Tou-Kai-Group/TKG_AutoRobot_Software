1f5762f85bdf8d5854ebb6c4e3c3a9f1964baa5e
Masaaki Hijikata <hijimasa@gmail.com>
2025-02-09 13:23:57 +0900

    [update] urg_object_detector algorithm

--- colcon_ws/src/tkg_urg_object_detector/tkg_urg_object_detector/detector.py
+++ colcon_ws/src/tkg_urg_object_detector/tkg_urg_object_detector/detector.py
@@ -7,7 +7,7 @@ import numpy as np
 from numba import jit
 
 @jit(nopython=True, cache=True)
-def cluster_points(ranges, angle_min, angle_increment):
+def cluster_points(ranges, first_scan, angle_min, angle_increment):
     initial_clusters = []
     current_cluster_size = 0
     x_sum = 0.0
@@ -16,61 +16,74 @@ def cluster_points(ranges, angle_min, angle_increment):
     first_y = 0.0
     current_x = -1.0
     current_y = -1.0
+    is_near_first_scan = False
+    x_ref = 0.0
+    y_ref = 0.0
 
     for i in range(len(ranges)):
         # 最大距離は射程範囲に合わせて調整
         if ranges[i] == np.inf or ranges[i] <= 0.1 or ranges[i] >= 10.0:
             continue
+        # 最初のスキャンと近い場合には除外（検出距離による）
+        if abs(ranges[i] - first_scan[i]) < 0.2:
+            is_near_first_scan = True
+            angle_ref = angle_min + i * angle_increment
+            x_ref = first_scan[i] * math.cos(angle_ref)
+            y_ref = first_scan[i] * math.sin(angle_ref)
+            continue
         angle = angle_min + i * angle_increment
         x = ranges[i] * math.cos(angle)
         y = ranges[i] * math.sin(angle)
+        # 最初のスキャンと近い場合には除外（位置基準による）
+        if is_near_first_scan:
+            if math.sqrt((x - x_ref) ** 2 + (y - y_ref) ** 2) < 0.05:
+                x_ref = x
+                y_ref = y
+                continue
         # CoREjp 2025 フィールド外に発射しないための特殊条件
         if y > 0 and ranges[i] >= 7.5:
             continue
-        if math.sqrt((x - current_x) ** 2 + (y - current_y) ** 2) > 0.05:
-            if current_cluster_size >= 3:
-                current_cluster_x = x_sum / current_cluster_size
-                current_cluster_y = y_sum / current_cluster_size
-                current_cluster_radius = math.sqrt((x - first_x) ** 2 + (y - first_y) ** 2) / 2.0
-                initial_clusters.append([current_cluster_x, current_cluster_y, current_cluster_radius])
-            first_x = x
-            first_y = y
-            x_sum = x
-            y_sum = y
-            current_cluster_size = 1
-        else:
-            x_sum += x
-            y_sum += y
-            current_cluster_size += 1
-        current_x = x
-        current_y = y
+        is_near_first_scan = False
+
+        is_cluster_detected = False
+        for i in range(len(initial_clusters)):
+            dist = math.sqrt((x - initial_clusters[i][0]) ** 2 + (y - initial_clusters[i][1]) ** 2)
+            if dist < 0.05 + initial_clusters[i][2] :
+                is_cluster_detected = True
+                current_cluster_x = (x + initial_clusters[i][0] * initial_clusters[i][3]) / (initial_clusters[i][3] + 1)
+                current_cluster_y = (y + initial_clusters[i][1] * initial_clusters[i][3]) / (initial_clusters[i][3] + 1)
+                current_cluster_radius = initial_clusters[i][2]
+                if dist > initial_clusters[i][2]:
+                    current_cluster_radius = dist
+                initial_clusters[i][0] = current_cluster_x
+                initial_clusters[i][1] = current_cluster_y
+                initial_clusters[i][2] = current_cluster_radius
+                initial_clusters[i][3] = initial_clusters[i][3] + 1
+                break
+        if not is_cluster_detected:
+            initial_clusters.append([x, y, 0.0, 1])
+
+    # すべてのスキャン処理が終わった後に、点の数が4以上のクラスタのみを残す
+    initial_clusters = [cluster for cluster in initial_clusters if cluster[3] > 3]
 
     clusters = []
     current_i = 0
     is_cluster_detected = False
     for i in range(len(initial_clusters)):
-        a_x = initial_clusters[current_i][0]
-        a_y = initial_clusters[current_i][1]
-        a_r = initial_clusters[current_i][2]
-        b_x = initial_clusters[i][0]
-        b_y = initial_clusters[i][1]
-        b_r = initial_clusters[i][2]
-        cluster_size = a_r + b_r + math.sqrt((a_x - b_x) ** 2 + (a_y - b_y) ** 2)
+        a = initial_clusters[current_i]
+        b = initial_clusters[i]
+        cluster_size = a[2] + b[2] + math.sqrt((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2)
         if (cluster_size >= 1.2 and i >= 1):
-            b_x = initial_clusters[i-1][0]
-            b_y = initial_clusters[i-1][1]
-            b_r = initial_clusters[i-1][2]
+            b = initial_clusters[i-1]
             is_cluster_detected = True
         if i == len(initial_clusters)-1:
-            b_x = initial_clusters[i][0]
-            b_y = initial_clusters[i][1]
-            b_r = initial_clusters[i][2]
+            b = initial_clusters[i]
             is_cluster_detected = True
         if is_cluster_detected:
-            cluster_x = (a_x + b_x) / 2.0
-            cluster_y = (a_y + b_y) / 2.0
-            cluster_radius = (a_r + b_r + math.sqrt((a_x - b_x) ** 2 + (a_y - b_y) ** 2)) / 2.0
-            if cluster_radius <= 0.6:
+            cluster_x = (a[0] + b[0]) / 2.0
+            cluster_y = (a[1] + b[1]) / 2.0
+            cluster_radius = (a[2] + b[2] + math.sqrt((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2)) / 2.0
+            if cluster_radius >= 0.15 and cluster_radius <= 0.6:
                 clusters.append([cluster_x, cluster_y, cluster_radius])
             is_cluster_detected = False
             current_i = i
@@ -103,12 +116,8 @@ class ScanProcessor(Node):
             self.get_logger().info('First scan recorded.')
             return
 
-        # Replace points within 0.05m of the first scan with -1.0m
-        diff = np.abs(ranges - self.first_scan)
-        ranges[diff <= 0.2] = np.inf
-
         # Cluster points that are not -1.0 and within 0.1m of each other
-        clusters = cluster_points(ranges, np.float64(msg.angle_min), np.float64(msg.angle_increment))
+        clusters = cluster_points(ranges, self.first_scan, np.float64(msg.angle_min), np.float64(msg.angle_increment))
 
         self.publish_clusters(clusters)
 
